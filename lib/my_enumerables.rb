c87c74c395698cf93ec4bf1c1e4610cabd8f1597
samual moahmed <sam-wal@windowslive.com>
2023-11-04 21:11:08 +0200

    Implement my_each, my_all, my_any and my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,34 @@
 module Enumerable
   # Your code goes here
+  def my_select(&block)
+    # implemented this method using the explicit block catching technique
+    return self unless block_given?
+    selected = []
+    for i in 0..self.length-1
+      selected << self[i] if block.call(self[i])
+    end
+    return selected
+  end
+
+  def my_all?(&block)
+    all_match = true
+    return all_match unless block_given?
+    for i in 0..self.length-1
+      all_match = block.call(self[i])
+      break unless all_match == true
+    end
+    return all_match
+  end
+
+  def my_any?(&block)
+    return true unless block_given?
+    any_match = false
+    for i in 0..self.length-1
+      any_match = block.call(self[i])
+      return any_match unless any_match ==  false
+    end
+    return any_match
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +37,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for i in 0..self.length-1
+      yield(self[i])
+    end
+    return self
+  end
 end
